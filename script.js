1b24b5a8b59c548a4d6ba564ec283cce8c75e66b
Codewithme <124165512+Inscoding@users.noreply.github.com>
2024-11-17 16:22:55 +0530

    script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ var w = c.width = window.innerWidth,
 		
 		opts = {
 			strings: [ 'BE', 'JOY FULL', 'RA HANYY!' ],
-			charSize: 25,
-			charSpacing: 30,
-			lineHeight: 20,
+			charSize: 30,
+			charSpacing: 35,
+			lineHeight: 40,
 			
 			cx: w / 2,
 			cy: h / 2,
